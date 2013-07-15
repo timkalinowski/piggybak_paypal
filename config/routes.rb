c5af256790e4f8652f0102418e1c28128e94879b
Anthony Collini <anthony@evolveartistic.com>
2013-07-15 16:13:27 -0400

    Rails 4 update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 PiggybakPaypal::Engine.routes.draw do
-  match "/express" => "paypal#express", :as => :paypal_express
-  match "/process" => "paypal#process_express", :as => :paypal_process
+  get "/express" => "paypal#express", :as => :paypal_express
+  get "/process" => "paypal#process_express", :as => :paypal_process
 end
